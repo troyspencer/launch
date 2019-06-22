336e35ed8a4563a30a78d668aa134f085a0653d3
troyspencer <troy.a.spencer@gmail.com>
2019-06-22 18:44:36 -0500

    Folded gameView and Overlay into Game

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react'
-import { Overlay } from './overlay';
+import Drawer from '@material-ui/core/Drawer';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import  { Stats } from "./stats/stats";
 
 export const Game = () => {
     const [paused, setPaused] = React.useState(false)
+    const [sidebarOpen, setSidebarOpen] = React.useState(true)
 
     React.useEffect(() => {
         const handleKey = (e: KeyboardEvent) => {
@@ -10,14 +15,45 @@ export const Game = () => {
                 setPaused(!paused)
             }
         }
-        
+
         window.addEventListener("keyup", handleKey);
         return () => {
             window.removeEventListener("keyup", handleKey);
         }
     }, [paused])
 
+    const vertical = useMediaQuery('(max-aspect-ratio:1/1)');
+
+    const theme = createMuiTheme({
+        palette: {
+          type: 'dark',
+        },
+    });
+
+    const styles = {
+        mycanvas: {
+            position: 'fixed',
+            backgroundColor: "black",
+            opacity: 1.0,
+            width: "100%",
+            height: "100%",
+            top:0,
+            right:0,
+            bottom:0,
+            left:0
+        } as React.CSSProperties
+    } 
+
     return (
-        <Overlay paused={paused} setPaused={setPaused} />
+        <ThemeProvider theme={theme}>
+            <canvas style={styles.mycanvas} id="mycanvas" />
+            <Drawer 
+                variant="persistent"
+                anchor={vertical ? "bottom" : "left"}   
+                open={sidebarOpen} 
+            >
+                <Stats paused={paused} setPaused={setPaused} />
+            </Drawer>
+        </ThemeProvider>
     );
 }
